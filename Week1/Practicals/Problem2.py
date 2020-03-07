c603d1ecf03dbbddadba7f2d7441a47d874f9abb
manukmanch <manukbarseghyan90@gmail.com>
2020-03-07 12:30:19 +0400

    first commit

---
+++ Week1/Practicals/Problem2.py
@@ -0,0 +1,4 @@
+AB = 3
+AC = 4
+BC = (AB**2 + AC**2)**(1/2)
+print("The hypotenuse of the triangle ABC = ", BC)
---
+++ Week1/Practicals/test.py
@@ -0,0 +1,11 @@
+course = "Intro to Python"
+student = "Harutyun Hakobyan"
+nickname = "Harut"
+grade = 100
+pythonExpert = True
+    
+print("course: " + course)
+print("student: " + student)
+print("nickname: " + nickname)
+print("grade:",grade)
+print("pythonExpert:",pythonExpert)
